7ed30a983cc3be6531ce266ead3fcfc13c1a2694
Karpe Aditya Mohan <Karpe Aditya Mohan@LAPTOP-GRP69F4I>
2023-04-01 23:33:59 +0530

    Add project files.

---
+++ InheritanceDemo/GrandParent.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceDemo
+{
+   //internal class GrandParent
+   // {
+   //     public void function1()
+   //     {
+
+   //     }
+   //     public void function2()
+   //     {
+
+   //     }
+   // }
+
+    internal class Parent
+    {
+        public void function3() 
+        {
+
+        }
+        public void function4()
+        {
+
+        }
+    }
+    internal class Child1: Parent
+    {
+        public void function5()
+        {
+
+        }
+        public void function6()
+        {
+
+        }
+    }
+    internal class Child2: Parent
+    {
+        public void function7()
+        {
+
+        }
+        public void  function8()
+        {
+
+        }
+    }
+}
---
+++ InheritanceDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using InheritanceDemo;
+
+
+// Single Inheritance (Parent - Child)
+
+// Multi-level Inheritance (GrandParent - Parent - Child)
+
+//Child c = new Child();
+//c.function5();
+//c.function6();
+
+//c.function3();     //we established relationship so we can call functions from parent class
+//c.function4();
+
+//c.function1();    
+//c.function2();
+
+
+// Hierarchical Inheritance  - Parents to all Childs
+
+Child1 c1 = new Child1();
+c1.function5();
+c1.function6();
+c1.function3();
+c1.function4();
+
+Child2 c2 = new Child2();
+c2.function7();
+c2.function8();
+c2.function3();
+c2.function4();
+
